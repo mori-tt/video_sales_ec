03712ab98522f3906e206791c72d9e8ecd642457
T_Mori <morit0301@gmail.com>
2024-07-23 23:23:04 +0900

    🎉 Setting up supabase and getting data from it

---
+++ app/[id]/page.tsx
@@ -0,0 +1,32 @@
+import { Database } from "@/lib/database.types";
+import {
+  SupabaseClient,
+  createServerComponentClient,
+} from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+const getDetailLesson = async (
+  id: number,
+  supabase: SupabaseClient<Database>
+) => {
+  const { data: lesson } = await supabase
+    .from("video_sales_ec")
+    .select("*")
+    .eq("id", id)
+    .single();
+  return lesson;
+};
+
+const LessonDetailPage = async ({ params }: { params: { id: number } }) => {
+  const supabase = createServerComponentClient<Database>({ cookies });
+  const lesson = await getDetailLesson(params.id, supabase);
+
+  return (
+    <div className="w-full max-w-3xl mx-auto py-16 px-8">
+      <h1 className="text-3xl mb-6">{lesson?.title}</h1>
+      <p className="mb-8">{lesson?.description}</p>
+    </div>
+  );
+};
+
+export default LessonDetailPage;
