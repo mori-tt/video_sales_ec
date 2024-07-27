bdd638c2f3823ad93f185a6d06d8d78eb9399678
T_Mori <morit0301@gmail.com>
2024-07-27 19:45:49 +0900

    🐛 tsconfig.json

--- utills/extractYoutubeVideoId.ts
+++ utills/extractYoutubeVideoId.ts
@@ -12,3 +12,16 @@ export function extractYouTubeVideoId(url: string): string | null {
     return null;
   }
 }
+
+// export function extractYouTubeVideoId(url: string): string | null {
+//   const matched =
+//     /^https?:\/\/(www\.)?youtube\.com\/watch\?(.*&)?v=([^&]+)/.exec(url) ??
+//     /^https?:\/\/youtu\.be\/([^?]+)/.exec(url) ??
+//     /^https?:\/\/(www\.)?youtube\.com\/embed\/([^?]+)/.exec(url);
+
+//   if (matched && matched[3]) {
+//     return matched[3];
+//   } else {
+//     return null;
+//   }
+// }
